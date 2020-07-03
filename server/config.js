fab0de85b2d827ccecd370758ebe63a7f6e47677
Shivam Dwivedi <38386339+imshivamdwivedi@users.noreply.github.com>
2020-07-03 12:39:39 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl':'mongodb+srv://mern123:mern123@cluster0-ru1na.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    'mongoUrl':'mongodb+srv://username:mern123@cluster0-ru1na.gcp.mongodb.net/test?retryWrites=true&w=majority'
 }
